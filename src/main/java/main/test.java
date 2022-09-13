5d1ab84de87a4dfcd86ab4ee30bbc57430b772d3
pavolskumaprospektykapitalizmu <sobotapavol9@gmail.com>
2022-09-13 07:09:31 +0200

    started working on mp3 to byte

--- src/main/java/main/test.java
+++ src/main/java/main/test.java
@@ -5,14 +5,36 @@ import javax.crypto.SecretKey;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.security.SecureRandom;
 
 public class test {
     public static void main(String[] args) {
         try {
-            SemanticUtils semanticUtils = new SemanticUtils();
-            semanticUtils.encryptToMIDIFromFile("image.jpg");
-            semanticUtils.decryptFromMIDIToFile("encryptedBytes","encryptionKey");
+//            SemanticUtils semanticUtils = new SemanticUtils();
+//            semanticUtils.encryptToMIDIFromFile("image.jpg");
+//            semanticUtils.decryptFromMIDIToFile("encryptedBytes","encryptionKey");
+
+//            File file = new File("song1.mp3"); // Sorrow.mp3 is the local mp3 music needs to be sent
+//            FileInputStream loc = new FileInputStream(file);
+//            byte[] rawMp3 = new byte[(int)file.length()];
+//            loc.read(rawMp3);
+//            System.out.println(rawMp3);
+//            File file2 = new File("song2.mp3");
+//            FileOutputStream fos = new FileOutputStream(file2);
+//            fos.write(rawMp3);
+//            fos.flush();
+//            fos.close();
+
+            byte my_byte = 3;
+            int pos = 0;
+//            my_byte = my_byte | (1 << );
+            System.out.println(my_byte);
+
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
