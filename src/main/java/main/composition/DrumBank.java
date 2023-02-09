7a96d390ab67d721877a002dc2e59398280b7d99
pavolskumaprospektykapitalizmu <sobotapavol9@gmail.com>
2023-02-09 08:45:15 +0100

    DrumBank is class that holds many drum patterns and adapts them to generated drum output

---
+++ src/main/java/main/composition/DrumBank.java
@@ -0,0 +1,37 @@
+package main.composition;
+
+public class DrumBank {
+    public DrumBank() {
+    }
+// empty drumset "................"
+    private String[] r1 = new String[] { "o..oo..o..oo.oo.o..oo..o..oo.oo.",
+        ".s.s..s.ss.s..s..s.s..s.ss.s..s.",
+        "................",
+        "................",
+        "................" };
+
+//    public static
+
+    public String[] fillLayers(String[] layers){
+        String[] finalLayers = new String[layers.length];
+        for(int i = 0; i < layers.length; i++){
+            StringBuilder newLayer = new StringBuilder();
+            char[] currentLayer = r1[i].toCharArray();
+            char[] inputLayer = layers[i].toCharArray();
+            int k = 0;
+
+            for (int j = 0; j < inputLayer.length; j++){
+                while (currentLayer[k]=='.' ){
+                    k++;
+                    newLayer.append('.');
+                }
+                k++;
+                newLayer.append(inputLayer[j]);
+            }
+            finalLayers[i] = newLayer.toString();
+        }
+        return finalLayers;
+    }
+
+
+}
